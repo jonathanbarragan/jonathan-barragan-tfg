337ee7c20924ffe1fc33c9c795ab95da4b379d88
Jonathan <jonathan.ba242@gmail.com>
2024-06-05 10:43:51 +0200

    ordenar por tiempo, precio

--- src/Components/Resultados.jsx
+++ src/Components/Resultados.jsx
@@ -3,30 +3,63 @@ import data from "../restaurantes.json";
 import {Image} from "react-bootstrap";
 import "./Resultados.css";
 import { Productos } from "./Productos";
+
 export const Resultados = ({search})=>{
     const restaurantes = data.restuarantes || [];
     const [selectedRestaurant, setSelectedRestaurant] = useState(null);
+    const [sortCriteria, setSortCriteria] = useState('none');
 
 
     const handleRestaurantClick = (restaurant) => {
       setSelectedRestaurant(restaurant);
     };
 
+    const handleSortChange = (e) => {
+      setSortCriteria(e.target.value);
+    };
+  
+    const sortData = (data) => {
+      switch (sortCriteria) {
+        case 'price_asc':
+          return data.sort((a, b) => a.min_price - b.min_price);
+        case 'price_desc':
+          return data.sort((a, b) => b.max_price - a.max_price);
+        case 'time':
+          return data.sort((a, b) => a.avg_time - b.avg_time);
+        default:
+          return data;
+      }
+    };
 
-    const filteredData =restaurantes.filter(restaurante => 
+    const filteredData = sortData( restaurantes.filter(restaurante => 
         restaurante.name.toLowerCase().includes(search.toLowerCase())
+      )
     );
 
     return(
       <div>
         {!selectedRestaurant ? (
-          <div className="resultados-container" >
-            {filteredData.map(item => (
-              <div className="card card-body shadow-lg" key={item.name} onClick={()=> handleRestaurantClick(item)}>
-                <h2>{item.name}</h2>
-                <p>{item.location}</p>
-              </div>
-            ))}
+           <div>
+            <div className="sort-dropdown">
+              <label htmlFor="sort">Ordenar por: </label>
+              <select id="sort" onChange={handleSortChange}>
+                <option value="none">Seleccionar</option>
+                <option value="price_asc">Precio: Más barato</option>
+                <option value="price_desc">Precio: Más caro</option>
+                <option value="time">Tiempo</option>
+              </select>
+            </div>
+
+            <div className="resultados-container" >
+              {filteredData.map(item => (
+                <div className="restaurante-card " key={item.name} onClick={()=> handleRestaurantClick(item)}>
+                  <h2>{item.name}</h2>
+                  <p>{item.location}</p>
+                  <p className="restaurante-price"> {item.min_price}€ - {item.max_price}€ | ~{item.avg_time}min </p>
+
+                </div>
+              ))}
+            </div>
           </div>
         ):(
           <Productos restaurant={selectedRestaurant} onBack={() => setSelectedRestaurant(null)} />
